e90fb974559b3dda480a90a390ceb9e2a105761b
hitmank <karan.b755@gmail.com>
2017-06-18 17:09:04 -0700

    first push
    first push

---
+++ myPage.js
@@ -0,0 +1,26 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+    	//delay(125);
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+(function($) {
+    $(document).ready(function() {
+        $.jScrollability([
+            {
+                'selector': '.slide-in-demo',
+                'start': 'parent',
+                'end': 'parent',
+                'fn': {
+                    'left': {
+                        'start': 100,
+                        'end': 0,
+                        'unit': '%'
+                    }
+                }
+            }
+        ]);
+    });
+})(jQuery);
