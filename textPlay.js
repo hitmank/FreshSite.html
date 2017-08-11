e01d01cb7e54b4a7a589c781bf1a044b57ea780e
hitmank <karan.b755@gmail.com>
2017-08-10 19:42:08 -0700

    1. Opacity in textplay background letters 2. Added content and sizing to My Work

--- textPlay.js
+++ textPlay.js
@@ -133,6 +133,7 @@ var meny = Meny.create({
     scrambleOthers();
     $('#imgWrapper').removeClass('animated fadeInRight')
     $('#imgWrapper').addClass('animated fadeOutRight')
+
     setTimeout(animateContainer,1000);
     setTimeout(function(){showImageForPage(currentPage)},1000);
     
@@ -141,11 +142,17 @@ var meny = Meny.create({
    * Functions
    */
    function animateContainer(){
-          $('#imgWrapper').removeClass('animated fadeOutRight')
+      $('#imgWrapper').removeClass('animated fadeOutRight')
       $('#imgWrapper').addClass('animated fadeInRight ')
+     
    }
    function showImageForPage(pageNumber){
     if (pageNumber == 0){
+       $('#descP').removeClass('pForLarge')
+
+       $('#descP').text('Space Commute is a iOS game made using GameKit. It is your classic space shooter, with multiple levels.\nFeeder is a app I am currently wokring on, that will aggregate search results from multiple sources like, Twitter, Facebook, Quora, various news channels etc. Perform some basic analysis and give you overall consolidated information about a topic.')
+        $('#descList').css('display','none')
+
       $('#soup-nav').removeClass('page1nav')
       $('#soup-nav').removeClass('page2nav')
       $('#soup-nav').removeClass('page3nav')
@@ -162,6 +169,8 @@ var meny = Meny.create({
       $('#workImage1').removeClass('smallerWidthThanParentDiv')
       $('#workImage1').addClass('smallerWidthThanParentDiv')
 
+      $('#workImage1').removeClass('sa1')
+      $('#workImage1').removeClass('fb1')
       $('#workImage1').removeClass('ios1')
       $('#workImage1').removeClass('sap1')
       $('#workImage1').removeClass('iris1')
@@ -174,6 +183,8 @@ var meny = Meny.create({
       $('#workImage2').addClass('smallerWidthThanParentDiv')
       // $('#workImage2').css('left','') 
       // $('#workImage2').css('width','42%') 
+      $('#workImage1').removeClass('sa2')
+      $('#workImage1').removeClass('fb2')
       $('#workImage2').removeClass('ios2')
       $('#workImage2').removeClass('sap2')
       $('#workImage2').removeClass('iris2')
@@ -193,7 +204,10 @@ var meny = Meny.create({
       $('#bannerImage').addClass('bannerImagePage1');
     }
     else if(pageNumber == 1){
+       $('#descP').removeClass('pForLarge')
 
+      $('#descP').text('For 2 and a half years, I worked as an iOS Developer for SAP Labs India.I worked on the SAP Business Intelligence iOS Application, promarily focusing on :')
+      $('#descList').css('display','inline')
       $('#soup-nav').removeClass('page1nav')
       $('#soup-nav').removeClass('page2nav')
       $('#soup-nav').removeClass('page3nav')
@@ -211,6 +225,8 @@ var meny = Meny.create({
       $('#workImage1').removeClass('smallerWidthThanParentDiv')
       $('#workImage1').addClass('largerWidthThanParentDiv')
 
+      $('#workImage1').removeClass('sa1')
+      $('#workImage1').removeClass('fb1')
       $('#workImage1').removeClass('ios1')
       $('#workImage1').removeClass('sap1')
       $('#workImage1').removeClass('iris1')
@@ -224,6 +240,8 @@ var meny = Meny.create({
       $('#workImage2').removeClass('smallerWidthThanParentDiv')
       $('#workImage2').addClass('smallerWidthThanParentDiv')
 
+      $('#workImage1').removeClass('sa2')
+      $('#workImage1').removeClass('fb2')
       $('#workImage2').removeClass('ios2')
       $('#workImage2').removeClass('sap2')
       $('#workImage2').removeClass('iris2')
@@ -244,7 +262,10 @@ var meny = Meny.create({
         
     }
     else if(pageNumber == 2){
-            $('#soup-nav').removeClass('page1nav')
+       $('#descP').removeClass('pForLarge')
+       $('#descP').text('During my final year of college at BMSIT, I worked on a project where we looked at the existing methods used in Iris Recognition and looked to find a way to improve how effect it is under situations where there is lot of occlusion.')
+       $('#descList').css('display','none')
+      $('#soup-nav').removeClass('page1nav')
       $('#soup-nav').removeClass('page2nav')
       $('#soup-nav').removeClass('page3nav')
       $('#soup-nav').addClass('page3nav')
@@ -258,6 +279,8 @@ var meny = Meny.create({
       $('#workImage1').removeClass('smallerWidthThanParentDiv')
       $('#workImage1').addClass('largerWidthThanParentDiv')
 
+      $('#workImage1').removeClass('sa1')
+      $('#workImage1').removeClass('fb1')
       $('#workImage1').removeClass('ios1')
       $('#workImage1').removeClass('sap1')
       $('#workImage1').removeClass('iris1')
@@ -271,6 +294,8 @@ var meny = Meny.create({
       $('#workImage2').removeClass('smallerWidthThanParentDiv')
       $('#workImage2').addClass('largerWidthThanParentDiv') 
 
+      $('#workImage1').removeClass('sa2')
+      $('#workImage1').removeClass('fb2')
       $('#workImage2').removeClass('ios2')
       $('#workImage2').removeClass('sap2')
       $('#workImage2').removeClass('iris2')
@@ -289,7 +314,10 @@ var meny = Meny.create({
       $('#bannerImage').addClass('bannerImagePage3');
     }
     else if(pageNumber == 3){
-            $('#soup-nav').removeClass('page1nav')
+       $('#descP').removeClass('pForLarge')
+      $('#descP').text('During my first semester at USC, I created a facebook search app for my Web Technologies class. For the front end, I used a combination of Bootstrap, Angular and Vanilla JS. For the Backend, it was PHP and hosted on AWS.')
+      $('#descList').css('display','none')
+      $('#soup-nav').removeClass('page1nav')
       $('#soup-nav').removeClass('page2nav')
       $('#soup-nav').removeClass('page3nav')
       $('#soup-nav').addClass('page3nav')
@@ -298,28 +326,32 @@ var meny = Meny.create({
       $('#alphabetContainer').removeClass('page3')
       $('#alphabetContainer').addClass('page3')
 
-      $('#workImage1').attr('src','img/iris1.jpg')
+      $('#workImage1').attr('src','img/fb11.png')
       $('#workImage1').removeClass('largerWidthThanParentDiv')
       $('#workImage1').removeClass('smallerWidthThanParentDiv')
       $('#workImage1').addClass('largerWidthThanParentDiv')
 
+      $('#workImage1').removeClass('sa1')
+      $('#workImage1').removeClass('fb1')
       $('#workImage1').removeClass('ios1')
       $('#workImage1').removeClass('sap1')
       $('#workImage1').removeClass('iris1')
-      $('#workImage1').addClass('iris1')
+      $('#workImage1').addClass('fb1')
 
 
-      $('#workImage2').attr('src','img/iris2.png')
+      $('#workImage2').attr('src','img/fb2.png')
       // $('#workImage2').css('width','65%')
       // $('#workImage2').css('left','18%') 
       $('#workImage2').removeClass('largerWidthThanParentDiv')
       $('#workImage2').removeClass('smallerWidthThanParentDiv')
       $('#workImage2').addClass('largerWidthThanParentDiv') 
 
+      $('#workImage1').removeClass('sa2')
+      $('#workImage1').removeClass('fb2')
       $('#workImage2').removeClass('ios2')
       $('#workImage2').removeClass('sap2')
       $('#workImage2').removeClass('iris2')
-      $('#workImage2').addClass('iris2')
+      $('#workImage2').addClass('fb2')
 
       $('#bannerLink').removeClass('bannerLinkPage1');
       $('#bannerLink').removeClass('bannerLinkPage1');
@@ -334,51 +366,10 @@ var meny = Meny.create({
       $('#bannerImage').addClass('bannerImagePage3');
     }
     else if(pageNumber == 4){
-            $('#soup-nav').removeClass('page1nav')
-      $('#soup-nav').removeClass('page2nav')
-      $('#soup-nav').removeClass('page3nav')
-      $('#soup-nav').addClass('page3nav')
-       $('#alphabetContainer').removeClass('page1')
-      $('#alphabetContainer').removeClass('page2')
-      $('#alphabetContainer').removeClass('page3')
-      $('#alphabetContainer').addClass('page3')
-
-      $('#workImage1').attr('src','img/iris1.jpg')
-      $('#workImage1').removeClass('largerWidthThanParentDiv')
-      $('#workImage1').removeClass('smallerWidthThanParentDiv')
-      $('#workImage1').addClass('largerWidthThanParentDiv')
-
-      $('#workImage1').removeClass('ios1')
-      $('#workImage1').removeClass('sap1')
-      $('#workImage1').removeClass('iris1')
-      $('#workImage1').addClass('iris1')
-
-
-      $('#workImage2').attr('src','img/iris2.png')
-      // $('#workImage2').css('width','65%')
-      // $('#workImage2').css('left','18%') 
-      $('#workImage2').removeClass('largerWidthThanParentDiv')
-      $('#workImage2').removeClass('smallerWidthThanParentDiv')
-      $('#workImage2').addClass('largerWidthThanParentDiv') 
-
-      $('#workImage2').removeClass('ios2')
-      $('#workImage2').removeClass('sap2')
-      $('#workImage2').removeClass('iris2')
-      $('#workImage2').addClass('iris2')
-
-      $('#bannerLink').removeClass('bannerLinkPage1');
-      $('#bannerLink').removeClass('bannerLinkPage1');
-      $('#bannerLink').removeClass('bannerLinkPage1');
-      $('#bannerLink').removeClass('bannerLinkPage4');
-      $('#bannerImage').removeClass('bannerImagePage1');
-      $('#bannerImage').removeClass('bannerImagePage2');
-      $('#bannerImage').removeClass('bannerImagePage3');
-      $('#bannerImage').removeClass('bannerImagePage4');
-
-      $('#bannerLink').addClass('bannerLinkPage3');
-      $('#bannerImage').addClass('bannerImagePage3');
-    }
-    else if(pageNumber == 5){
+       $('#descP').removeClass('pForLarge')
+       $('#descP').addClass('pForLarge')
+      $('#descP').text('For my Artificial Intelligence class, I built a Sentiment Analysis engine on Mathematica, using a 1/5th test to train ratio. The data was amazon reviews of various products and the goal was to classify the reviews as either positive or negative. The dataset was obtained from Kaggle. The preprocessing of the data included multiple tasks such as converting to lower case, removal of stop words, etc. Followed by the training phase, where we used Neural Nets, Decision trees and Naive Bayes. We compared the results of the various algorithms and produced various visualisations.');
+      $('#descList').css('display','none')
       $('#soup-nav').removeClass('page1nav')
       $('#soup-nav').removeClass('page2nav')
       $('#soup-nav').removeClass('page3nav')
@@ -388,28 +379,32 @@ var meny = Meny.create({
       $('#alphabetContainer').removeClass('page3')
       $('#alphabetContainer').addClass('page3')
 
-      $('#workImage1').attr('src','img/iris1.jpg')
+      $('#workImage1').attr('src','img/sa1.png')
       $('#workImage1').removeClass('largerWidthThanParentDiv')
       $('#workImage1').removeClass('smallerWidthThanParentDiv')
       $('#workImage1').addClass('largerWidthThanParentDiv')
 
+      $('#workImage1').removeClass('sa1')
+      $('#workImage1').removeClass('fb1')
       $('#workImage1').removeClass('ios1')
       $('#workImage1').removeClass('sap1')
       $('#workImage1').removeClass('iris1')
-      $('#workImage1').addClass('iris1')
+      $('#workImage1').addClass('sa1')
 
 
-      $('#workImage2').attr('src','img/iris2.png')
+      $('#workImage2').attr('src','img/sa2.png')
       // $('#workImage2').css('width','65%')
       // $('#workImage2').css('left','18%') 
       $('#workImage2').removeClass('largerWidthThanParentDiv')
       $('#workImage2').removeClass('smallerWidthThanParentDiv')
       $('#workImage2').addClass('largerWidthThanParentDiv') 
 
+      $('#workImage2').removeClass('sa2')
+      $('#workImage2').removeClass('fb2')
       $('#workImage2').removeClass('ios2')
       $('#workImage2').removeClass('sap2')
       $('#workImage2').removeClass('iris2')
-      $('#workImage2').addClass('iris2')
+      $('#workImage2').addClass('sa2')
 
       $('#bannerLink').removeClass('bannerLinkPage1');
       $('#bannerLink').removeClass('bannerLinkPage1');
@@ -423,6 +418,7 @@ var meny = Meny.create({
       $('#bannerLink').addClass('bannerLinkPage3');
       $('#bannerImage').addClass('bannerImagePage3');
     }
+   
    }
   function arrangeCurrentPage() {
     for (var i = 0; i < content[currentPage].title.length; i++) {
@@ -488,7 +484,7 @@ var meny = Meny.create({
               top: randTop,
                
              zIndex: "initial",
-             opacity: "0.4"
+             opacity: "0.1"
             });
           }
           else{
@@ -497,7 +493,7 @@ var meny = Meny.create({
               top: randTop,
               // color: "",
              zIndex: "initial",
-             opacity: "0.4"
+             opacity: "0.1"
             });
 
 
